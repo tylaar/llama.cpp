436d07bc3045c11236cbb755a7b2c2d107e88453
Yifeng Yu <tylaar@yeah.net>
2023-04-08 15:29:28 +0800

    change param

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-./llama -m ~/hack/models/llama/7B/ggml-model-q4_0.bin -p "who is Donald Trump?" -n 512 -n 512
+if ! [[ "$1" =~ ^[0-9]{1,2}B$ ]]; then
+    echo
+    echo "Usage: run.sh 7B|13B|30B|65B"
+    echo
+    exit 1
+fi
+
+
+./llama -m ~/hack/models/llama/$1/ggml-model-q4_0.bin -p $2 -n 512 -n 512
